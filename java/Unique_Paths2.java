27361c560de5adb48cfc0dabadbd6216392fb642
Masker Tim <s110516026@stu.ntue.edu.tw>
2020-01-25 01:25:25 +0800

    Unique Paths 2 added notation

--- java/Unique_Paths2.java
+++ java/Unique_Paths2.java
@@ -1,6 +1,7 @@
 public class Unique_Paths2{
     public static int uniquePathsWithObstacles(int[][] obstacleGrid) {
         int[][] dp = new int[obstacleGrid.length+1][obstacleGrid[0].length+1];
+        // 若該列上有障礙及該障礙以後的，則設置為0
         for(int i=0;i<obstacleGrid.length;i++){
             if(obstacleGrid[i][0]==1){
                 dp[i][0]=0;
@@ -8,6 +9,7 @@ public class Unique_Paths2{
                 break;
             }else{dp[i][0]=1;}
         }
+        // 若該欄上有障礙及該障礙以後的，則設置為0
         for(int i=0;i<obstacleGrid[0].length;i++){
             if(obstacleGrid[0][i]==1){
                 dp[0][i]=0;
@@ -16,6 +18,7 @@ public class Unique_Paths2{
             }else{dp[0][i]=1;}
         }
 
+        // 若該格有障礙，則設置為0；以dynamic programming演算法計算
         for(int i=1;i<obstacleGrid.length;i++){
             for(int j=1;j<obstacleGrid[0].length;j++){
                 if(obstacleGrid[i][j]==1){dp[i][j]=0;}
